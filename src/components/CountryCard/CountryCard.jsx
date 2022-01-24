0981f48f6b5924e8ca4ec1c1e9b2e18c1988cbc9
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-01-24 17:37:22 +0200

    init

---
+++ src/components/CountryCard/CountryCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const CountryCard = ({ data }) => {
+    const countryData = data;
+    const countryName = countryData?.name;
+    const region = countryData?.region;
+    const size = countryData?.size;
+
+    return(
+        <div></div>
+    )
+  
+}
