2786f8db68660753369d8630a02f1d5b0e126d6e
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-01-24 18:18:00 +0200

    Create country card

--- src/components/CountryCard/CountryCard.jsx
+++ src/components/CountryCard/CountryCard.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import './country-card.scss'; 
 
-export const CountryCard = ({ data }) => {
-    const countryData = data;
-    const countryName = countryData?.name;
-    const region = countryData?.region;
-    const size = countryData?.size;
-
+export const CountryCard = ({countryName, countryRegion, countryArea}) => {
+    
     return(
-        <div></div>
+        <div className="country-card">
+            <span className="country-card__name">NAME: {countryName}</span>
+            <span className="country-card__region">REGION: {countryRegion}</span>
+            <span className="country-card__area">SIZE: {countryArea}</span>
+        </div>
     )
   
 }
